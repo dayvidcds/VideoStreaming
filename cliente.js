8ae201e4d46db99a1e7249da1757d073e2353b30
Dayvid <dayvid.blender@gmail.com>
2018-04-15 17:34:20 -0300

    Update cliente.js

--- cliente.js
+++ cliente.js
@@ -3,6 +3,13 @@ var io = require('socket.io-client')
 var myHostname = 'no02.com'
 var myIPaddr = '1.1.1.2'
 
+app.use(function(req, res, next) {
+    // console.log(req.connection.remoteAddress)
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
 var socket = io('http://localhost:4000');
   socket.on('connect', function () {
 	console.log( myHostname + '|' +  myIPaddr +' connected to server')
